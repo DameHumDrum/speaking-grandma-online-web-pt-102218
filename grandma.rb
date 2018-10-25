6b46c685645b54b8af9bf5b8f01100616b55ab11
Katie Ashlin <akka726@outlook.com>
2018-10-25 18:51:57 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,4 +15,6 @@ def speak_to_grandma(phrase.upcase)
   phrase = "I love you grandma!"
   if phrase == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN"
-  elsif
+  elsif phrase == "Hey Grandma"
+  puts "HUH!? SPEAK UP SONNY"
+  
